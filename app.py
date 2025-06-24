650098ce472ac28d7e4b89f6b56efc4e08ce34d7
Max Stevenson <maxstevensonpoore92@gmail.com>
2025-06-23 19:29:26 -0700

    Debugging enabled

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def upload_history():
 # Run the app
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
